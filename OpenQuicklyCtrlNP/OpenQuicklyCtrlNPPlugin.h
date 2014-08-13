a2e86db0f7547af10f997b0626a42ef7bc92c99b
tyeen <ptyeen@gmail.com>
2014-08-13 09:16:12 +0900

    Init commit.

---
+++ OpenQuicklyCtrlNP/OpenQuicklyCtrlNPPlugin.h
@@ -0,0 +1,10 @@
+//
+//  Created by Yin Tan on 8/13/14.
+//  Copyright (c) 2014 Yin Tan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OpenQuicklyCtrlNPPlugin : NSObject
+
+@end
---
+++ OpenQuicklyCtrlNP/OpenQuicklyCtrlNPPlugin.m
@@ -0,0 +1,15 @@
+//
+//  Created by Yin Tan on 8/13/14.
+//  Copyright (c) 2014 Yin Tan. All rights reserved.
+//
+
+#import "OpenQuicklyCtrlNPPlugin.h"
+
+@implementation OpenQuicklyCtrlNPPlugin
+
++ (void)pluginDidLoad:(NSBundle *)plugin
+{
+    NSLog(@"open quickly ctrlnp loaded.");
+}
+
+@end
